cf62d48d1ea19fb59bfc61bc573e2fc0ae34e43a
Tom <t_hankinson@outlook.com>
2022-09-30 12:15:04 +0100

    final commit

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
-from flask import url_for
+from flask import url_for, redirect
 from flask_testing import TestCase
 from application import app
 
@@ -17,9 +17,23 @@ class TestNum(TestBase):
        
         assert isTrue
 
-class TestPage(TestBase):
-    def is_index(self):
-        response = TestBase.client.get(url_for('index'))
-        # self.assertEqual(response.status_code, 200)
-        # isTrue = self.assertIn()
-        assert b'Simple number Add' in response.data
+class TestIndex(TestBase):
+
+    def test_index_page(self):
+        response = self.client.get(url_for('index'))
+        self.assertEqual(response.status_code, 200)
+
+class TestIndexContentTitle(TestBase):
+    
+    def test_index_has_h1_title(self):
+        response = self.client.get(url_for('index'))
+        html = response.data.decode()
+
+        assert "<h3>Simple number Add</h3>" in html
+
+class TestIndexContentInput(TestBase):
+    def test_index_has_input_field(self):
+        response = self.client.get(url_for('index'))
+        html = response.data.decode()
+
+        assert '<input type="number" name="num1" placeholder="Enter num1" id="">' in html
